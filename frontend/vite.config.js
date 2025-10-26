13b147ed83948bedf645d54f5021f7e5c373528a
Roman <itz.ruzik@vk.com>
2025-10-26 16:28:12 +0000

    fnsh

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/mobdev-lab13-filatov/'
+    plugins: [
+        react(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            devOptions: {
+                enabled: true,
+            },
+            manifestFilename: 'manifest.json',
+            includeManifestIcons: false,
+        }),
+    ],
 })
